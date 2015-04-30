d44cc04dc27834a4fb93251f3c0efe3c8f5d9d4d
Alex Luckett <alexgluckett@gmail.com>
2015-04-30 13:44:03 +0100

    Updated genome serialiser

---
+++ Biomorphs/src/model/FileSerializer.java
@@ -0,0 +1,49 @@
+package model;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+
+/**
+ * Serialises objects of type T.
+ * 
+ * @author Alex Luckett
+ * 
+ * @param <T> type of class
+ */
+public class FileSerializer<T> {
+
+	public FileSerializer() { } // doesn't need instantiating
+	
+	public void serialiseFile(T object, String path) throws IOException {
+		FileOutputStream fileOutput = new FileOutputStream(path);
+		ObjectOutputStream objectOutput = new ObjectOutputStream(fileOutput);
+		
+		objectOutput.writeObject(object);
+		
+		objectOutput.close();
+		fileOutput.close();
+	}
+	
+	@SuppressWarnings("unchecked") // only way to work around this unfortunately. still safe as it will throw exception.
+	public T deserialiseFile(String path) throws IOException, ClassNotFoundException {
+		FileInputStream fileInput = new FileInputStream(path);
+        ObjectInputStream objectInput = new ObjectInputStream(fileInput);
+        
+        T object = null;
+        
+        try {
+        	object = (T) objectInput.readObject();
+        } finally {
+        	objectInput.close();
+            fileInput.close();
+        }
+        
+        return object;
+	}
+	
+
+	
+}
